f4e70faac9508c9c84215162d25b806bb5a733b6
Chaitanya <cxk340@case.edu>
2023-07-04 00:04:57 -0400

    Update visualization script.

--- view_stitched_stack.py
+++ view_stitched_stack.py
@@ -1,29 +1,38 @@
 import zarr
 import matplotlib.pyplot as plt
 from skimage import exposure
+import numpy as np
+import cv2
 
 plt.ion()
 
 # Inputs
-zarr_filename = r'C:\MUSE datasets\REVA\Polymerization Tests\S8\MUSE_stitched_acq_2.zarr'
-slices = 1000
+zarr_filename = r'C:\MUSE datasets\REVA\Polymerization Tests\S4\MUSE_stitched_acq_2.zarr'
 skip = 100
 
-gamma = 0.6
+gamma = 0.75
 vmin = 0
 vmax = 2500
 
 # Visualization
 zarr_file = zarr.open(zarr_filename)
-muse_data = zarr_file['muse/stitched']
+muse_data = zarr_file['muse']
 
-for k in range(0, slices, skip):
-    image = muse_data[k, :, :]
+print('Dataset shape:')
+print(muse_data.shape)
+
+for k in range(0, muse_data.shape[0], skip):
+    image = np.squeeze(muse_data[k, :, :, :])
+
     image[image > vmax] = vmax
+    image[image < vmin] = vmin
 
-    image = exposure.adjust_gamma(image, gamma)
+    image = cv2.normalize(image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 
-    plt.imshow(image, cmap='gray', vmin = vmin, vmax= vmax)
+    image = exposure.adjust_gamma(image, gamma = gamma) 
+
+
+    plt.imshow(image, cmap='gray')
     plt.show()
     plt.pause(1)
     plt.clf()
